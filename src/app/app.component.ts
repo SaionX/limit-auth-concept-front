ecd1ac348d403867a42dfb3e7724814c66317bde
sion <sion.andreu@gmail.com>
2021-01-31 13:30:58 +0100

    Modificacions login amb paràmetres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AuthConfig, NullValidationHandler, OAuthService } from "angular-oauth2-oidc";
+import {HttpHeaders} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'limit-auth-concept-front';
+  headers : HttpHeaders;
+  identificador: string;
+  empresa: string;
+  usuari: string;
+  rols: string[];
+
+  constructor(private oauthService: OAuthService) {
+    this.configure();
+  }
+
+  ngOnInit(): void {
+    this.oauthService.events.subscribe(event => {
+      if (event.type == 'token_received' || event.type == 'token_refreshed') {
+        this.userData();
+      }
+    })
+  }
+
+  authConfig: AuthConfig = {
+    issuer: 'http://localhost:8081/auth/realms/limit',
+    redirectUri: window.location.origin + "/",
+    clientId: 'limit-auth-concept',
+    dummyClientSecret: '15c1373e-5087-4b58-9f09-f11c15617b33',
+    scope: 'authentication_code',
+    responseType: 'code',
+    // at_hash is not present in JWT token
+    disableAtHashCheck: true,
+    showDebugInformation: true
+  }
+
+  public login() {
+    this.oauthService.initLoginFlow();
+  }
+
+  public logoff() {
+    this.oauthService.logOut();
+  }
+
+  public refresh() {
+    this.oauthService.customQueryParams = {
+      'limit-identificador': this.identificador,
+      'limit-empresa': this.empresa
+    };
+    this.oauthService.refreshToken().then((resp) => {
+      // this.userData();
+    });
+  }
+
+  private userData() {
+    let claims = this.oauthService.getIdentityClaims();
+    console.log('Claims:', claims);
+    console.log('Usuari:', claims['preferred_username']);
+    console.log('Rols:', claims['realm_access']['roles']);
+
+    if (claims) {
+      this.usuari = claims['preferred_username'];
+      this.rols = claims['realm_access']['roles'];
+    }
+  }
+
+  private configure() {
+    this.oauthService.configure(this.authConfig);
+    this.oauthService.tokenValidationHandler = new  NullValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { OAuthModule } from "angular-oauth2-oidc";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -22,7 +27,18 @@ import { HomeComponent } from './home/home.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['http://localhost:9090/api', 'http://localhost:8083/api'],
+        sendAccessToken: true
+      }
+    }),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
